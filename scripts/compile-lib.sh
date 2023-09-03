de216b33d69b0d2e9722cf6c8d8fd5182495ff3a
Royce Remulla <royce.com@gmail.com>
2023-09-03 21:11:28 +0800

    🏗 build: Default to deploy under "core"

--- scripts/compile-lib.sh
+++ scripts/compile-lib.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-INPUT_FILE_PATH=$(echo "$1 $2 $3" | sed 's/ *$//')
+# Change Logs:
+# 	September 3, 2023 3:58 PM - Use packages.
+
+INPUT_FILE_PATH=$(echo "$1 $2 $3 $4 $5" | sed 's/ *$//')
 # echo "INPUT_FILE_PATH: [$INPUT_FILE_PATH]"
 
 BASEFILENAME=$(echo $INPUT_FILE_PATH | awk -F/ '{print $NF}' | sed 's/ *$//')
 # echo "BASEFILENAME: [$BASEFILENAME]"
 
-osacompile -o ~/Library/Script\ Libraries/"$BASEFILENAME.scpt" "${INPUT_FILE_PATH}.applescript"
+osacompile -o ~/Library/Script\ Libraries/core/"$BASEFILENAME.scpt" "${INPUT_FILE_PATH}.applescript"
