19a730f36ebd7600c7e0c9dd8c7677a8761be2d0
Royce Remulla <royce.com@gmail.com>
2025-12-06 13:42:08 +0800

    🏗 build: Fix the test library deployment

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -4,6 +4,10 @@
 # 	Compile a passed AppleScript file and deploy to the user's Script Libraries/core sub directory.
 #
 # @Change Logs:
+#   Tue, Nov 25, 2025, at 02:39:49 PM
+#       - Use the all_arguments $*.
+#       - Detect test scripts and deploy under <script libraries>/core/test
+
 # 	Thu, Jul 18, 2024 at 10:06:00 PM - Use deployment type.
 # 	September 3, 2023 3:58 PM - Use packages.
 
@@ -12,25 +16,30 @@ PLIST_PATH="$HOME/applescript-core/session.plist"
 DEPLOY_TYPE=$(plutil -extract "$DEPLOY_TYPE_KEY" raw "$PLIST_PATH")
 # echo "DEBUG: Deployment Type: [$DEPLOY_TYPE]"
 
-input_file_path=$(echo "$1 $2 $3 $4 $5" | sed 's/ *$//')
+all_arguments="$*"
+input_file_path=${all_arguments// *$//}
 # echo "DEBUG: input_file_path: [$input_file_path]"
 
 base_filename=$(echo "$input_file_path" | awk -F/ '{print $NF}' | sed 's/ *$//')
 # echo "DEBUG: base_filename: [$base_filename]"
 
-deployment_path="$HOME/Library/Script Libraries/core/"
+deployment_path="$HOME/Library/Script Libraries/core"
 if [[ $DEPLOY_TYPE != *"error"* ]]; then
     if [[ $DEPLOY_TYPE == "computer" ]]; then
-        deployment_path="/Library/Script Libraries/core/"
+        deployment_path="/Library/Script Libraries/core"
     fi
 # else
 #     echo "DEBUG: Deployment type was not configured, using defaults"
 fi
 
+deploy_subpath=""
+if [[ "$all_arguments" == test/* ]]; then
+    deploy_subpath="/test"
+fi
 
-# osacompile -o "$deployment_path$base_filename.scpt" "${input_file_path}.applescript"
+# NOTE: Compiling directly to the deployment target results in permission error.
+# Using a staging directory resolves that issue.
+staging_directory="/tmp"
+osacompile -o "${staging_directory}/${base_filename}.scpt" "${input_file_path}.applescript"
 
-staging_directory="/tmp/"
-osacompile -o "$staging_directory$base_filename.scpt" "${input_file_path}.applescript"
-
-mv "$staging_directory$base_filename.scpt" "$deployment_path$base_filename.scpt"
+mv "${staging_directory}/${base_filename}.scpt" "${deployment_path}/${deploy_subpath}/${base_filename}.scpt"
